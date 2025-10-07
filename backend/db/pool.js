c4b9861904d0616267f912825a12b8d6889882af
VladislavSCV <vladnety134@gmail.com>
2025-10-07 14:16:30 +0300

    очень полезный коммент

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || "postgresql://kursach_user:rmAlkg1FeNrZgUtqWSmziDGy8q2G3ZWq@dpg-d3fs69p5pdvs73bq34j0-a.oregon-postgres.render.com/kursachdb",
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
